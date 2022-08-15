0289fed7763d0e1969508b6dbd0e97ca030250cd
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-15 16:37:43 +0900

    add: 共通レイアウトコンポーネントを作成

---
+++ src/components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <div className="w-screen flex justify-center items-center flex-col min-h-screen font-mono bg-black">
+      <Head>
+        <title>コードバトル</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+      <main className="flex flex-1 justify-start items-center w-screen flex-col">
+        {children}
+      </main>
+    </div>
+  );
+}
