ea09290aa10a7f9a7a90548e839d6c38cd8ae32b
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-17 11:00:59 +0900

    update: onComplete props を追加した

--- src/features/play/components/SimpleCountdown.tsx
+++ src/features/play/components/SimpleCountdown.tsx
@@ -4,13 +4,14 @@ import Countdown, { CountdownRendererFn } from "react-countdown";
 interface Props {
   time: number;
   renderer: CountdownRendererFn | undefined;
+  onComplete?: () => void;
 }
 interface ChildHandles {
   start: () => void;
   pause: () => void;
 }
 const SimpleCountdown = forwardRef<ChildHandles, Props>(
-  ({ time, renderer }, ref) => {
+  ({ time, renderer, onComplete }, ref) => {
     const clockRef = useRef();
     useImperativeHandle(ref, () => {
       return {
@@ -25,6 +26,7 @@ const SimpleCountdown = forwardRef<ChildHandles, Props>(
           renderer={renderer}
           autoStart={false}
           ref={clockRef}
+          onComplete={onComplete}
         />
       </div>
     );
