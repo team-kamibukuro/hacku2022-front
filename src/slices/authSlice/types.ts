813e29991a0b37115610e36148a015fed5e6a4fd
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-17 16:38:10 +0900

    add: 型定義

--- src/slices/authSlice/types.ts
+++ src/slices/authSlice/types.ts
@@ -7,3 +7,25 @@ export interface AuthState {
   isAuthChecking: boolean;
   currentUser: User;
 }
+
+export interface LoginRequest {
+  userEmail: string;
+  userPassword: string;
+}
+
+export interface LoginResponse {
+  status: number;
+  userId: string;
+  userName: string;
+}
+
+export interface RegisterRequest {
+  userEmail: string;
+  userPassword: string;
+  userName: string;
+}
+
+export interface RegisterResponse {
+  userId: string;
+  userName: string;
+}
