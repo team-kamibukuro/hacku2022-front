8539e0595ddbe7bf08680090c65f65b958b7d5eb
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-17 10:58:06 +0900

    add: StartGame component

---
+++ src/features/play/components/dialogContents/StartGame.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import StartGameCountdown from "../StartGameCountdown";
+
+const StartGame = () => {
+  return (
+    <div>
+      <div className="text-center px-5 w-80 flex flex-col items-center">
+        <p className="font-dot mb-3">まもなくゲームが始まります。</p>
+        <p className="font-dot mb-3">ゲーム開始まで残り</p>
+        <StartGameCountdown />
+      </div>
+    </div>
+  );
+};
+
+export default StartGame;
