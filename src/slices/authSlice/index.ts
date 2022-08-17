d1ff61e4115cc94968d8cf17d725a53d1041faaa
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-17 16:39:17 +0900

    add: login register api logic

--- src/slices/authSlice/index.ts
+++ src/slices/authSlice/index.ts
@@ -1,6 +1,30 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "@/store";
-import { AuthState } from "./types";
+import {
+  AuthState,
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse,
+} from "./types";
+import client from "@/common/utils/client";
+import { AxiosResponse } from "axios";
+
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/login",
+  async (auth: LoginRequest) => {
+    const res = await client.post<LoginResponse>("/login", auth);
+    return res;
+  }
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: RegisterRequest) => {
+    const res = await client.post<RegisterResponse>("auth/signup", auth);
+    return res;
+  }
+);
 
 const initialState: AuthState = {
   isAuthChecking: true,
@@ -14,6 +38,40 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchAsyncLogin.fulfilled,
+      (state, action: PayloadAction<AxiosResponse<LoginResponse>>) => {
+        localStorage.setItem(
+          "localJWT",
+          action.payload.headers["Authorization"]
+        );
+        state.isAuthChecking = false;
+        state.currentUser.id = action.payload.data.userId;
+        state.currentUser.name = action.payload.data.userName;
+        window.location.href = "/entrance";
+      }
+    );
+    builder.addCase(fetchAsyncLogin.rejected, (e) => {
+      console.log(e);
+    });
+    builder.addCase(
+      fetchAsyncRegister.fulfilled,
+      (state, action: PayloadAction<AxiosResponse<RegisterResponse>>) => {
+        localStorage.setItem(
+          "localJWT",
+          action.payload.headers["Authorization"]
+        );
+        state.isAuthChecking = false;
+        state.currentUser.id = action.payload.data.userId;
+        state.currentUser.name = action.payload.data.userName;
+        window.location.href = "/entrance";
+      }
+    );
+    builder.addCase(fetchAsyncRegister.rejected, (e) => {
+      console.log(e);
+    });
+  },
 });
 
 // export const { editCurrentUser, editRoom } = authSlice.actions;
