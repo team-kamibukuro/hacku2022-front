03deb4c2beb5373c3cc855a50b14e1223f6e953a
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-17 11:02:01 +0900

    add: 型定義を追加

---
+++ src/features/play/types/index.ts
@@ -0,0 +1,30 @@
+export const Event = {
+  CONNECT: "CONNECT",
+  DISCONNECT: "DISCONNECT",
+  PLAYER: "PLAYER",
+  UPDATE_CODE: "UPDATE_CODE",
+  UPDATE_HEART: "UPDATE_HEART",
+  FINISHED: "FINISHED",
+  ATTACK: "ATTACK",
+  FIREWALL: "FIREWALL",
+} as const;
+
+export type EventType = typeof Event[keyof typeof Event];
+
+export const Attack = {
+  INDENT_INJECTION: "INDENT_INJECTION",
+  COMMENTOUT_INJECTION: "COMMENTOUT_INJECTION",
+  TBC_POISONING2: "TBC_POISONING2",
+};
+
+export type AttackType = typeof Attack[keyof typeof Attack];
+
+export const DialogEvent = {
+  MatchingWaiting: 0,
+  StartGame: 1,
+  ServerError: 2,
+  Finish: 3,
+  None: 4,
+} as const;
+
+export type DialogEventType = typeof DialogEvent[keyof typeof DialogEvent];
