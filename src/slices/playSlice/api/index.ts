7d30e74629a4760b625b9464aeb448a6485c7fbf
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-19 11:59:14 +0900

    fix: headerを付与

--- src/slices/playSlice/api/index.ts
+++ src/slices/playSlice/api/index.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import client from "@/common/utils/client";
-import { AxiosResponse } from "axios";
 import {
   AuthRoomRequest,
   AuthRoomResponse,
@@ -11,7 +10,11 @@ import {
 export const fetchAsyncCreateRoom = createAsyncThunk(
   "play/createRoom",
   async (room: CreateRoomRequest) => {
-    const res = await client.post<CreateRoomResponse>("/room", room);
+    const res = await client.post<CreateRoomResponse>("/room", room, {
+      headers: {
+        Authorization: `${localStorage.localJWT}`,
+      },
+    });
     return res.data;
   }
 );
@@ -19,7 +22,11 @@ export const fetchAsyncCreateRoom = createAsyncThunk(
 export const fetchAsyncAuthRoom = createAsyncThunk(
   "play/authRoom",
   async (room: AuthRoomRequest) => {
-    const res = await client.post<AuthRoomResponse>("/room/get", room);
+    const res = await client.post<AuthRoomResponse>("/room/get", room, {
+      headers: {
+        Authorization: `${localStorage.localJWT}`,
+      },
+    });
     return res.data;
   }
 );
