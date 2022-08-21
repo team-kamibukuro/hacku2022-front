43954f876f653559af97217f9c7d555bddb2d3f9
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-21 14:25:15 +0900

    add: FINISHED 表示style

--- src/features/play/components/UserInfo.tsx
+++ src/features/play/components/UserInfo.tsx
@@ -3,26 +3,36 @@ import React from "react";
 interface Props {
   name: string;
   heartbeat: number;
+  finished: boolean;
 }
 
-const UserInfo: React.FC<Props> = ({ name, heartbeat }) => {
+const UserInfo: React.FC<Props> = ({ name, heartbeat, finished }) => {
   return (
-    <div className="flex items-center">
-      <p className="font-dot text-white text-base mr-2 mb-1">{name}</p>
-      {heartbeat < 1 ? (
-        <Heart isTransparent={true} />
-      ) : (
-        <Heart isTransparent={false} />
-      )}
-      {heartbeat < 2 ? (
-        <Heart isTransparent={true} />
-      ) : (
-        <Heart isTransparent={false} />
-      )}
-      {heartbeat < 3 ? (
-        <Heart isTransparent={true} />
-      ) : (
-        <Heart isTransparent={false} />
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
+        <p className="font-dot text-white text-base mr-2 mb-1">{name}</p>
+        {heartbeat < 1 ? (
+          <Heart isTransparent={true} />
+        ) : (
+          <Heart isTransparent={false} />
+        )}
+        {heartbeat < 2 ? (
+          <Heart isTransparent={true} />
+        ) : (
+          <Heart isTransparent={false} />
+        )}
+        {heartbeat < 3 ? (
+          <Heart isTransparent={true} />
+        ) : (
+          <Heart isTransparent={false} />
+        )}
+      </div>
+      {finished && (
+        <div className="bg-white px-1 border-solid border-red-700 border-2">
+          <p className="font-press text-sm text-red-500 ml-2">
+            FNISHED!!<span className="text-lg">🎉</span>
+          </p>
+        </div>
       )}
     </div>
   );
