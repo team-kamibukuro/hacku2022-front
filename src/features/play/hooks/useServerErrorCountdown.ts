e2dcedfb5e521d9535fb3440e558ebf203743745
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-22 15:49:07 +0900

    add: send event: 500_ERROR

--- src/features/play/hooks/useServerErrorCountdown.ts
+++ src/features/play/hooks/useServerErrorCountdown.ts
@@ -1,17 +1,27 @@
 import { resetDialog, resetHeart, selectCurrentUser } from "@/slices/playSlice";
+import { sendWebsocket } from "@/slices/websocketSlice";
 import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { Event } from "../types";
 
 const useServerErrorCountdown = () => {
   const clockRef = useRef();
-  const refFirstRef = useRef(true);
+  const currentUser = useSelector(selectCurrentUser);
 
   const dispatch = useDispatch();
 
   const onComplete = () => {
     dispatch(resetDialog());
     dispatch(resetHeart());
+    dispatch(
+      sendWebsocket({
+        event: Event.SERVER_ERROR,
+        status: false,
+        playerId: currentUser.id,
+        name: currentUser.name,
+      })
+    );
   };
 
   return { clockRef, onComplete };
