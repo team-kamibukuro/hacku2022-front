ca1dda76e3266fa36e1ef8a156c787809ffaba9f
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-22 14:15:07 +0900

    add: startCountdown flag

--- src/features/play/components/dialogContents/StartGame.tsx
+++ src/features/play/components/dialogContents/StartGame.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import useStartGame from "../../hooks/useStartGame";
 import StartGameCountdown from "../StartGameCountdown";
+import { RotateLoader as Loader } from "react-spinners";
 
 const StartGame = () => {
+  const { startCountdown } = useStartGame();
+
   return (
     <div>
-      <div className="text-center px-5 w-80 flex flex-col items-center">
-        {/* <p className="font-dot mb-3">まもなくゲームが始まります。</p> */}
-        <p className="font-dot mb-3">ゲーム開始まで残り</p>
-        <StartGameCountdown />
-      </div>
+      {startCountdown ? (
+        <div className="text-center px-5 w-80 flex flex-col items-center">
+          <p className="font-dot mb-3">ゲーム開始まで残り</p>
+          <StartGameCountdown />
+        </div>
+      ) : (
+        <div className="text-center px-5 w-80 flex flex-col items-center">
+          <div className="mb-12 mt-6">
+            <Loader color={"#ffff"} loading={true} size={10} />
+          </div>
+          <p className="font-dot mb-3">まもなくゲームが始まります。</p>
+        </div>
+      )}
     </div>
   );
 };
