0f19980ccbadb378b2f65ccab5d3313d0d78e122
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-23 01:04:58 +0900

    fix: 初回ロード時に音楽が流れない問題

--- src/features/entrance/routes/Entrance.tsx
+++ src/features/entrance/routes/Entrance.tsx
@@ -10,6 +10,7 @@ import InputRoomNameForm from "../components/InputRoomNameForm";
 import MatchingForm from "../components/MatchingForm";
 import useDialog from "../hooks/useDialog";
 import useIsDisabled from "../hooks/useIsDisabled";
+import useTouchStart from "../hooks/useTouchStart";
 
 export const Entrance = () => {
   const {
@@ -23,6 +24,7 @@ export const Entrance = () => {
     handleClickClose,
   } = useDialog();
   const { isDisabled } = useIsDisabled(access);
+  const { touchStart, touch } = useTouchStart();
 
   const formComponents = {
     0: InputRoomNameForm,
@@ -34,64 +36,83 @@ export const Entrance = () => {
   useEntranceBackGroundSound();
 
   return (
-    <Layout>
-      <StainedGlass />
-      <div className="h-screen flex flex-col items-center justify-center z-50">
-        <div className="p-20 border-white border-solid border-8">
-          <div className="flex items-center justify-center ">
-            <h2 className="text-white text-[80px] font-press mr-10 border-white border-solid border-b-8">
+    <div>
+      <Layout>
+        <StainedGlass />
+        {touchStart ? (
+          <div
+            onClick={touch}
+            className="h-screen flex flex-col items-center justify-center z-50"
+          >
+            <h2 className="text-white text-2xl font-press border-white border-solid border-b-4 mb-10">
               INGECTION
             </h2>
-            <div className="flex flex-col">
-              <Button
-                onClick={input}
-                buttonStyle={ButtonStyle.isWarning}
-                font={"press"}
-              >
-                Input room name
-              </Button>
-              <div className="my-2"></div>
-              <Button
-                onClick={create}
-                buttonStyle={ButtonStyle.isWarning}
-                font={"press"}
-              >
-                Create room
-              </Button>
-              <div className="my-2"></div>
-              <Button
-                onClick={matching}
-                buttonStyle={ButtonStyle.isWarning}
-                font={"press"}
-              >
-                Matching
-              </Button>
-              <SimpleDialog
-                handleClose={handleClickClose}
-                handleClick={handleClick}
-                open={open}
-                dialogTitle={dialogTitle}
-                submitTitle={"Enter"}
-                isDisabled={isDisabled}
-              >
-                <Form />
-              </SimpleDialog>
+            <p className="text-white text-5xl font-press text-center mb-20 leading-normal">
+              TOUCH HERE
+              <br />
+              <span className="text-4xl">TO START</span>
+            </p>
+            <p className="text-white font-dot">※このゲームは音楽が流れます</p>
+          </div>
+        ) : (
+          <div className="h-screen flex flex-col items-center justify-center z-50">
+            <div className="p-20 border-white border-solid border-8">
+              <div className="flex items-center justify-center ">
+                <h2 className="text-white text-[80px] font-press mr-10 border-white border-solid border-b-8">
+                  INGECTION
+                </h2>
+                <div className="flex flex-col">
+                  <Button
+                    onClick={input}
+                    buttonStyle={ButtonStyle.isWarning}
+                    font={"press"}
+                  >
+                    Input room name
+                  </Button>
+                  <div className="my-2"></div>
+                  <Button
+                    onClick={create}
+                    buttonStyle={ButtonStyle.isWarning}
+                    font={"press"}
+                  >
+                    Create room
+                  </Button>
+                  <div className="my-2"></div>
+                  <Button
+                    onClick={matching}
+                    buttonStyle={ButtonStyle.isWarning}
+                    font={"press"}
+                  >
+                    Matching
+                  </Button>
+                  <SimpleDialog
+                    handleClose={handleClickClose}
+                    handleClick={handleClick}
+                    open={open}
+                    dialogTitle={dialogTitle}
+                    submitTitle={"Enter"}
+                    isDisabled={isDisabled}
+                  >
+                    <Form />
+                  </SimpleDialog>
+                </div>
+              </div>
+              <Link href="/mypage">
+                <div className="flex mt-10 group">
+                  <span className="font-press text-lg -mt-0.5 text-white hover:text-yellow-400">
+                    Mypage
+                  </span>
+                </div>
+              </Link>
+              <div className="flex mt-10 group">
+                <span className="font-press text-lg -mt-0.5 text-white hover:text-yellow-400">
+                  Logout
+                </span>
+              </div>
             </div>
           </div>
-          <Link href="/mypage">
-            <div className="flex mt-10 group">
-              <span className="font-press text-lg -mt-0.5 text-white hover:text-yellow-400">
-                Mypage
-              </span>
-            </div>
-          </Link>
-          <div className="flex mt-10 group">
-            <span className="font-press text-lg -mt-0.5 text-white hover:text-yellow-400">
-              Logout
-            </span>
-          </div>
-        </div>
-      </div>
-    </Layout>
+        )}
+      </Layout>
+    </div>
   );
 };
