a93f7a8feba157821782bb898d28d341951441bd
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-23 17:15:39 +0900

    update: isDemo, MaxPlayer Select Form

--- src/features/entrance/components/CreateRoomForm.tsx
+++ src/features/entrance/components/CreateRoomForm.tsx
@@ -1,18 +1,31 @@
+import Checkbox from "@/components/ui-elements/Checkbox";
 import InputForm from "@/components/ui-elements/InputForm";
 import React from "react";
 import useRoomHandleChange from "../hooks/useRoomHandleChange";
+import DemoCheckBox from "./DemoCheckBox";
 import SelectLanguageForm from "./SelectLanguageForm";
+import SelectMaxPlayerForm from "./SelectMaxPlayerForm";
 
 const CreateRoomForm = () => {
-  const { handleChange } = useRoomHandleChange();
+  const { room, handleChange } = useRoomHandleChange();
 
   return (
     <div>
       <div className="mt-6">
-        <InputForm label={"RoomName"} handleChange={handleChange} />
+        <InputForm
+          label={"RoomName"}
+          handleChange={handleChange}
+          value={room.name}
+        />
       </div>
       <div className="my-6">
         <SelectLanguageForm />
+      </div>
+      <div className="my-6">
+        <SelectMaxPlayerForm />
+      </div>
+      <div className="mb-6 mt-10">
+        <DemoCheckBox />
       </div>
     </div>
   );
