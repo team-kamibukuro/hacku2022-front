3d2c78a92711b9f04669db33ecac50349d15d434
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-24 23:01:12 +0900

    add: Radio component

---
+++ src/components/ui-elements/Radio/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react";
+import styles from "./style.module.css";
+
+interface Item {
+  value: string;
+  label: string;
+}
+
+interface Props {
+  name: string;
+  items: Item[];
+  state: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Radio: React.FC<Props> = (props) => {
+  const { name, items, state, onChange } = props;
+  return (
+    <>
+      {items.map((item, index) => (
+        <label key={index}>
+          <input
+            type="radio"
+            name={name}
+            value={item.value}
+            checked={item.value === state}
+            onChange={onChange}
+            className="nes-radio is-dark"
+          />
+          <span className={`font-dot ${styles.label__span}`}>{item.label}</span>
+        </label>
+      ))}
+    </>
+  );
+};
+
+export default Radio;
