efeae2cb634763d8fa32240bf1c033c9aefb2318
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-25 00:56:38 +0900

    update: typo

--- src/features/mypage/routes/Mypage.tsx
+++ src/features/mypage/routes/Mypage.tsx
@@ -49,7 +49,7 @@ export const Mypage = () => {
                   buttonStyle={ButtonStyle.isNomal}
                   font={"press"}
                 >
-                  Gyide
+                  Guide
                 </Button>
               </div>
             </div>
