e82dcb871c7f6076a708c9d5335fd20e140f98ee
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-25 06:31:42 +0900

    #102 デフォルト値設定

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -15,3 +15,210 @@ export const LANGUAGES = [
   "swift",
   "typescript",
 ];
+
+export const TEMPLATE = [
+  {
+    language: "c",
+    template: `// This file is a "Hello, world!" in C language by gcc for INJECTION.
+  #include <stdio.h>
+  #include <stdlib.h>
+  
+  int main(void)
+  {
+      puts("Hello, INJECTION!");
+      return EXIT_SUCCESS;
+  }
+  
+  // GCC reference:
+  //   https://gcc.gnu.org/
+  
+  // C language references:
+  //   https://msdn.microsoft.com/library/fw5abdx6.aspx
+  //   https://www.gnu.org/software/gnu-c-manual/`,
+  },
+  {
+    language: "csharp",
+    template: `// This file is a "Hello, world!" in C# language by Mono for INJECTION.
+    using System;
+    
+    namespace Injection
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, INJECTION!");
+            }
+        }
+    }
+    
+    // Mono references:
+    //   http://www.mono-project.com/
+    
+    // C# language references:
+    //   https://msdn.microsoft.com/library/618ayhy6.aspx`,
+  },
+  {
+    language: "cpp",
+    template: `// This file is a "Hello, world!" in C++ language by GCC for INJECTION.
+    #include <iostream>
+    #include <cstdlib>
+    
+    int main()
+    {
+        std::cout << "Hello, INJECTION!" << std::endl;
+    }
+    
+    // GCC reference:
+    //   https://gcc.gnu.org/
+    
+    // C++ language references:
+    //   https://cppreference.com/
+    //   https://isocpp.org/
+    //   http://www.open-std.org/jtc1/sc22/wg21/
+    
+    // Boost libraries references:
+    //   https://www.boost.org/doc/`,
+  },
+  {
+    language: "go",
+    template: `// This file is a "Hello, world!" in Go language for INJECTION.
+    package main
+    
+    import (
+            "fmt"
+    )
+    
+    func main() {
+            fmt.Println("Hello, INJECTION!")
+    }
+    
+    // Go language references:
+    //   https://golang.org/pkg/`,
+  },
+  {
+    language: "java",
+    template: `// This file is a "Hello, world!" in Java language by OpenJDK for INJECTION.
+
+    class Injection
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("Hello, Injection!");
+        }
+    }
+    
+    // OpenJDK reference:
+    //   http://openjdk.java.net/
+    
+    // Java language references:
+    //   http://docs.oracle.com/javase`,
+  },
+  {
+    language: "javascript",
+    template: `// This file is a "Hello, world!" in JavaScript by Node.js for INJECTION.
+
+    console.log("Hello, INJECTION!");
+    
+    // Node.js reference:
+    //   https://nodejs.org/
+    
+    // JavaScript language references:
+    //   http://www.ecma-international.org/publications/standards/Ecma-262.htm
+    //   https://developer.mozilla.org/docs/Web/JavaScript
+    //   https://msdn.microsoft.com/library/d1et7k7c(v=vs.94).aspx
+    `,
+  },
+  {
+    language: "php",
+    template: `<?php
+    // This file is a "Hello, world!" in PHP for INJECTION.
+    print("Hello, INJECTION!\n");
+    // PHP references:
+    //   http://php.net
+    `,
+  },
+  {
+    language: "perl",
+    template: `# This file is a "Hello, world!" in Perl language for INJECTION.
+
+    print "Hello, INJECTION!\n";
+    
+    # Perl language references:
+    #   http://perldoc.perl.org/
+    `,
+  },
+  {
+    language: "python",
+    template: `# This file is a "Hello, world!" in Python language by CPython for INJECTION.
+
+    print("Hello, INJECTION!")
+    
+    # CPython references:
+    #   https://www.python.org/`,
+  },
+  {
+    language: "r",
+    template: `# This file is a "Hello, world!" in R language for INJECTION.
+
+    data(iris)                               ## the "hello, world" of statistics.
+    summary(iris)                            ## a simple summary of the columns
+    fit <- lm(Sepal.Length ~ . , data=iris)  ## basic regression
+    summary(fit)                             ## summary
+    
+    cat("All done\n")
+    
+    # R language references:
+    #   https://cran.r-project.org/manuals.html`,
+  },
+  {
+    language: "ruby",
+    template: `# This file is a "Hello, world!" in Ruby language by ruby for INJECTION.
+
+    puts "Hello, INJECTION!"
+    
+    # Ruby reference:
+    #   https://docs.ruby-lang.org/`,
+  },
+  {
+    language: "rust",
+    template: `// This file is a "Hello, world!" in Rust language for INJECTION.
+
+    fn main()
+    {
+        println!("Hello, INJECTION!");
+    }
+    
+    // Rust language references:
+    //   https://www.rust-lang.org/
+    `,
+  },
+  {
+    language: "scala",
+    template: `// This file is a "Hello, world!" in Scala language for INJECTION.
+
+    object Injection {
+      def main(args: Array[String]): Unit = {
+        println("Hello, INJECTION!")
+      }
+    }
+    
+    // Scala language references:
+    //   http://www.scala-lang.org
+    `,
+  },
+  {
+    language: "swift",
+    template: `// This file is a "Hello, world!" in Apple Swift language for INJECTION.
+    print("Hello, INJECTION!");
+    
+    // Apple Swift language references:
+    //   http://www.apple.com/swift/`,
+  },
+  {
+    language: "typescript",
+    template: `const n: string = "Hello, INJECTION!";
+    console.log(n);
+    `,
+  },
+];
--- src/features/play/components/OwnEditor.tsx
+++ src/features/play/components/OwnEditor.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Editor from "@monaco-editor/react";
 import useOwnEditor from "../hooks/useOwnEditor";
+import { TEMPLATE } from "@/common/constants";
 
 const OwnEditor = () => {
   const { currentUser, handleEditorChange, handleEditorDidMount } =
     useOwnEditor();
+  const template = TEMPLATE.find(
+    (template) => template.language === currentUser.language
+  );
+
   return (
     <Editor
       height="100%"
@@ -13,6 +18,7 @@ const OwnEditor = () => {
       value={currentUser.code}
       onChange={handleEditorChange}
       onMount={handleEditorDidMount}
+      defaultValue={template?.template}
     />
   );
 };
