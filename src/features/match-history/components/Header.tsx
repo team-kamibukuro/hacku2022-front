6fdb5ecad54fe15403cd2499e882d278e9353bb3
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-24 23:01:54 +0900

    add: Header component

---
+++ src/features/match-history/components/Header.tsx
@@ -0,0 +1,27 @@
+import Button, { ButtonStyle } from "@/components/ui-elements/Button";
+import CustomCountdown from "@/features/play/components/CustomCountdown";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg py-2 relative flex items-center w-full justify-between border-solid border-white border-b-2 bg-black bg-opacity-75">
+          <div className="px-10 w-full flex flex-wrap items-center justify-between">
+            <h2 className="font-press text-gray-300">INJECTION</h2>
+            <div
+              className="navbar-collapse collapse grow items-center flex justify-end"
+              id="navbarSupportedContentY"
+            >
+              <Button onClick={() => {}} buttonStyle={ButtonStyle.isError}>
+                Back
+              </Button>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
