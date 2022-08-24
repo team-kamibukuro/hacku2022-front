ebce8921d9be087f93aac00ddd16771707d6907e
下村芽生 <mei.shimomura.hal@gmail.com>
2022-08-24 22:31:35 +0900

    add: mypage layout component

---
+++ src/components/layouts/SubPage.tsx
@@ -0,0 +1,23 @@
+import React, { Children } from "react";
+
+type Props = {
+  children?: React.ReactNode;
+  title: string;
+};
+const SubPage: React.FC<Props> = ({ children, title }) => {
+  return (
+    <div className="h-screen flex flex-col items-start justify-between z-50 py-20 w-[580px]">
+      <div className="w-full">
+        <div className="border-white border-solid border-b-4 w-full mb-5">
+          <h2 className="text-white text-2xl font-press">INJECTION</h2>
+        </div>
+        <h3 className="text-white text-4xl font-press mb-5 leading-normal">
+          {title}
+        </h3>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default SubPage;
