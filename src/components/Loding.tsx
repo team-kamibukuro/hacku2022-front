f794ac616f5d491e599859847bde0120508f41b7
下村芽生 <mei.shimomura@icloud.com>
2022-08-26 17:25:08 +0900

    update: loading

--- src/components/Loding.tsx
+++ src/components/Loding.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { ClimbingBoxLoader } from "react-spinners";
 
 const Loding = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen w-full flex items-center justify-center flex-col">
+      <p className="text-white font-press mb-10">Loding now...</p>
+      <ClimbingBoxLoader color={"#ffff"} loading={true} size={20} />
+    </div>
+  );
 };
 
 export default Loding;
